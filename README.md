b417b5e0a18c707f583adacc35c531a665aa8b61
Baer <baer.bradford@gmail.com>
2025-01-23 08:38:19 -0600

    making it really publish

--- README.md
+++ README.md
@@ -1,20 +1,27 @@
 # publish-game
-GitHub Action for publishing WebGL games.
+
+GitHub Action for publishing WebGL games to [Final Parsec](https://www.finalparsec.com).
+
+Relies on the Final Parsec API documented here: https://www.finalparsec.com/docs.
 
 ## Inputs
 
-## `who-to-greet`
+## `game-name`
 
-**Required** The name of the person to greet. Default `"World"`.
+**Required** The name of the game to publish. Default `"Untitled Game"`.
+
+## `asset-directory`
+
+**Required** The directory containing assets to be published. Default `"/fp-publish"`
 
 ## Outputs
 
 ## `time`
 
-The time we greeted you.
+The time the game was published.
 
 ## Example usage
 
-uses: Final-Parsec/publish-game@v2
+uses: Final-Parsec/publish-game@v1
 with:
-  who-to-greet: 'Mona the Octocat'
+  game-name: 'Aurora Tower Defense'
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,52 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+echo "Game name is $1"
+
+if [ ! -d "$2" ]; then
+  echo "$2 does not exist. Ensure directory containing publishable assets is configured and exists."
+  exit 1
+fi
+
+ls $2
+
+PAYLOAD_FILE="payload.json"
+
+# Start the JSON payload
+echo '{ "game": { "name": "' > $PAYLOAD_FILE
+echo $1 > $PAYLOAD_FILE
+echo '", "assets": [' > $PAYLOAD_FILE
+
+# Iterate over all files in the directory
+FIRST=true
+for FILE in "$2"/*; do
+  BASE64_DATA="data:application/octet-stream;base64,$(base64 -i "$FILE")"
+  FILENAME=$(basename "$FILE")
+  
+  # Add a comma before each entry except the first
+  if [ "$FIRST" = true ]; then
+    FIRST=false
+  else
+    echo ',' >> $PAYLOAD_FILE
+  fi
+
+  # Append the asset object to the JSON payload
+  cat <<EOF >> $PAYLOAD_FILE
+    {
+      "filename": "$FILENAME",
+      "data": "$BASE64_DATA"
+    }
+EOF
+done
+
+# Close the JSON payload
+echo '  ] } }' >> $PAYLOAD_FILE
+
+# POST to Final Parsec
+curl -X POST https://www.finalparsec.com/api/games \
+-H "Content-Type: application/json" \
+-d @payload.json
+
+
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 
